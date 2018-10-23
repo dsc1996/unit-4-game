061201eaca5cb1fa2c68438aa9abdb6245305b3e
Dustin Crawford <crawforddustins@yahoo.com>
2018-10-23 17:58:45 -0500

    Adding Images and Adding Points to Crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,5 +6,45 @@ var min=19;
 var max=120; 
 var random =Math.floor(Math.random() * (+max - +min)) + +min; 
 $(".rN").append(random); 
+});
 
-})
+$(document).ready(function(){
+    var min=1; 
+    var max=12; 
+    var value =Math.floor(Math.random() * (+max - +min)) + +min; 
+        $(document).on("click", ".crystal1", function(){
+            $(".userNum").append(value);
+        })
+   console.log(value);
+});
+
+$(document).ready(function(){
+    var min=1; 
+    var max=12; 
+    var value =Math.floor(Math.random() * (+max - +min)) + +min; 
+        $(document).on("click", ".crystal2", function(){
+            $(".userNum").append(value);
+        })
+   console.log(value);
+});
+
+$(document).ready(function(){
+    var min=1; 
+    var max=12; 
+    var value =Math.floor(Math.random() * (+max - +min)) + +min; 
+        $(document).on("click", ".crystal3", function(){
+            $(".userNum").append(value);
+        })
+   console.log(value);
+});
+
+$(document).ready(function(){
+    var min=1; 
+    var max=12; 
+    var value =Math.floor(Math.random() * (+max - +min)) + +min; 
+        $(document).on("click", ".crystal4", function(){
+            $(".userNum").append(value);
+        })
+   console.log(value);
+});
+
